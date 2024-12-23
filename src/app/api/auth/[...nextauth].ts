768f6aebf1f09a073c7c08e266b3f7cfc81ad3dc
Piyush khare <piyushkhare671@gmail.com>
2024-12-23 06:20:37 +0530

    route change

---
+++ src/app/api/auth/[...nextauth].ts
@@ -0,0 +1,16 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.AUTH_GOOGLE_ID as string,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET as string,
+    }),
+  ],
+  secret: process.env.AUTH_SECRET,
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };

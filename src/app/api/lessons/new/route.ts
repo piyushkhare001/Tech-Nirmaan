8f29c4cd15293bf79a0095b2470b15dd71e5b722
Piyush khare <piyushkhare671@gmail.com>
2025-05-15 22:35:42 +0530

    comment it out

--- src/app/api/lessons/new/route.ts
+++ src/app/api/lessons/new/route.ts
@@ -1,10 +1,10 @@
-// import  connectToDB  from '@/lib/mongodb';
-// import { Lesson } from '@/models/lesson';
-// import { NextResponse } from 'next/server';
+import  connectToDB  from '@/lib/mongodb';
+import { Lesson } from '@/models/lesson';
+import { NextResponse } from 'next/server';
 
-// export async function POST(req: Request) {
-//   await connectToDB();
-//   const body = await req.json();
-//   const lesson = await Lesson.create(body);
-//   return NextResponse.json(lesson);
-// }
+export async function POST(req: Request) {
+  await connectToDB();
+  const body = await req.json();
+  const lesson = await Lesson.create(body);
+  return NextResponse.json(lesson);
+}

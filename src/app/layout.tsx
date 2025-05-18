6d2d8a87c90de76a94af9f4622e851ee3ae536e7
Piyush khare <piyushkhare671@gmail.com>
2025-05-18 17:04:35 +0530

    removed seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,11 +6,6 @@
 
 import "./globals.css";
 
-import { NextSeo } from 'next-seo'; 
-import { DEFAULT_SEO } from '../../seo.config';
-import Head from "next/head";
-
-
 
 export default function RootLayout({
   children,
@@ -19,16 +14,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-        
-    <Head>
-  <title>Master DSA & MERN Stack | Technirmaan - Best Coding Courses Online</title>
-  <meta name="description" content="Join India's top-rated DSA & MERN Stack courses. Get curated DSA sheets, live mentoring, and project-based learning to become job-ready. Enroll today!" />
-  <meta name="keywords" content="DSA course, MERN stack training, coding sheets, data structures algorithms, web development course, JavaScript, React, Node.js" />
-</Head>
-    
       <body className="bg-black">
         
-       <NextSeo {...DEFAULT_SEO} />
+      
           {children}
       
       </body>
